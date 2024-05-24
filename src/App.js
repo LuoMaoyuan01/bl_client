88f5c2f8d664ace381b1b881980b118a61da6a64
luohr <luohr180@outlook.com>
2024-05-24 15:49:48 +0800

    Modify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+// Import styling files
 import './App.css';
 
+// Import required libraries & functions
+import React, { Component, useEffect, useState } from 'react';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'; // Install components, router, route
+import axios from axios
+
+// Utilizing react hooks
+const [data, setData] = useState();
+
 function App() {
+  useEffect(() => {
+    axios.get("http://localhost:5000/main/70").then((response) => {
+      setData(response);
+    })
+  }, [])
   return (
-    <div>Hello world</div>
-  );
+    <div>{data}</div>
+  );  
 }
 
 export default App;
