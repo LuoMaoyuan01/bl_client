9df77572f5e38ae0e676d29c8a6c040ccc81df6d
luohr <luohr180@outlook.com>
2024-05-28 13:41:05 +0800

    Add radio btn and integrated it into home page

--- src/components/ui/radioBtn/homeRadioBtn.js
+++ src/components/ui/radioBtn/homeRadioBtn.js
@@ -2,23 +2,22 @@
 import './homeRadioBtn.css';
 
 // Import required library and functions
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
-const RadioBtn = () => {
-
+const RadioBtn = ({radioInputValue, setRadioInputValue}) => {
 
 
     return (
-        <div class="radio-input">
+        <div className="radio-input">
             <label>
-                <input value="value-1" name="value-radio" id="value-1" type="radio"/>
+                <input value="1" name="value-radio" id="value-1" type="radio" checked={radioInputValue === "1"} onChange={() => setRadioInputValue("1")}/>
             <span>1</span>
             </label>
             <label>
-                <input value="value-2" name="value-radio" id="value-2" type="radio"/>
+                <input value="2" name="value-radio" id="value-2" type="radio" checked={radioInputValue === "2"} onChange={() => setRadioInputValue("2")}/>
             <span>2</span>
             </label>
-            <span class="selection"></span>
+            <span className="selection"></span>
         </div>
     );
 }
--- src/pages/Home/homepage.js
+++ src/pages/Home/homepage.js
@@ -9,19 +9,23 @@ import RadioBtn from "../../components/ui/radioBtn/homeRadioBtn";
 
 // Import required library and functions
 import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
 
 function HomePage() {
 
     const navigate = useNavigate();
+    const [radioInputValue, setRadioInputValue] = useState("1")
 
-
-    const handleSearchBtnSubmit = (inputValue) => {navigate(`/googlemaps/${inputValue}`)}
+    const handleSearchBtnSubmit = (inputValue) => {
+        console.log(`/googlemaps/${inputValue}/${radioInputValue}`);
+        navigate(`/googlemaps/${inputValue}/${radioInputValue}`)
+    };
 
     return(
         <div>
-            <HomePageSearchBtn handleSubmit={handleSearchBtnSubmit}/>
-            <RadioBtn/>
+            <HomePageSearchBtn handleSubmit={handleSearchBtnSubmit}/>   
+            <RadioBtn radioInputValue={radioInputValue} setRadioInputValue={setRadioInputValue}/>
         </div>
     )
 }
