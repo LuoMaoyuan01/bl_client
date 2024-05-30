b2b3039ebc50df1aa255d8a007dac045520a1094
luohr <luohr180@outlook.com>
2024-05-30 17:58:43 +0800

    style: Update reverseBtn.js to include img

--- src/components/ui/buttons/reverseBtn.js
+++ src/components/ui/buttons/reverseBtn.js
@@ -1,14 +1,17 @@
 // Import styling files
 import Styles from './reverseBtn.module.css';
 
+// Import images
+import reverseIcon from '../../../assets/images/reverseIcon.png';
+
 // Import required libraries and functions
 
 const ReverseBtn = () => {
 
 
     return(
-        <button className={`${Styles.button} ${Styles['full-rounded']}`}>
-            <span>Hover me</span>
+        <button className={`${Styles.button} ${Styles['full-rounded']}`}> 
+            <img src={reverseIcon}/>
             <div className={`${Styles.border} ${Styles['full-rounded']}`}></div>
         </button>
     );
