688e5fb035e0c014f1b4a6a90a15f5f5b84796f6
luohr <luohr180@outlook.com>
2024-05-31 09:29:06 +0800

    Created responsive reverseBtn and implemented into googlemaps UI

--- src/components/ui/buttons/reverseBtn.js
+++ src/components/ui/buttons/reverseBtn.js
@@ -6,15 +6,16 @@ import reverseIcon from '../../../assets/images/reverseIcon.png';
 
 // Import required libraries and functions
 
-const ReverseBtn = () => {
+const ReverseBtn = ({toggleBusDirection}) => {
 
 
     return(
-        <button className={`${Styles.button} ${Styles['full-rounded']}`}> 
+        <button className={`${Styles.button} ${Styles['full-rounded']}`} onClick={toggleBusDirection}> 
             <img src={reverseIcon}/>
             <div className={`${Styles.border} ${Styles['full-rounded']}`}></div>
         </button>
     );
 }
 
+// Button is responsive on all screen sizes
 export default ReverseBtn;  
--- src/pages/Map/googlemaps.js
+++ src/pages/Map/googlemaps.js
@@ -6,51 +6,43 @@ import ReverseBtn from '../../components/ui/buttons/reverseBtn';
 
 // Import required library and functions
 import React, {useState, useEffect} from 'react';
-import {useParams} from "react-router-dom";
+import {useParams, useNavigate} from "react-router-dom";
 import axios from 'axios';  
 import MapComponent from '../../components/api/googleMapsController';
 
 function Maps() {
-
     const { busNumber } = useParams();
     const { direction } = useParams();
-
-
+    const navigate = useNavigate();
+    const [busDirection, setBusDirection] = useState(direction || "1");
     const [busStops, setBusStops] = useState([]);
 
+    // Reloads data every time busNumber or busDirection values change
     useEffect(() => {
-        axios.get(`http://localhost:5000/scrape/${busNumber}/${direction}`)
+        axios.get(`http://localhost:5000/scrape/${busNumber}/${busDirection}`)
         .then((response) => {
-            const data = response.data
+            const data = response.data;
             setBusStops(data);
         })
         .catch((error) => {
             console.log(error);
-        })
-    }, [busNumber])
+        });
+    }, [busNumber, busDirection]);
 
     console.log(busStops);
 
+    // Function that toggles the busDirection value between 1 & 2
+    const toggleBusDirection = () => {
+        const newDirection = busDirection === "1" ? "2" : "1";
+        setBusDirection(newDirection);
+        navigate(`/googlemaps/${busNumber}/${newDirection}`); // Adjust the path accordingly
+    };
 
     return(
-    // ----------------------- FOR TESTING ------------------------
-    // <div>
-    //   <h1>Bus Stops for Bus Number: {busNumber}</h1>
-    //   <ul>
-    //     {busStops.map((stop, index) => (
-    //       <li key={index}>
-    //         <p>Road Name: {stop['Road Name']}</p>
-    //         <p>Bus Stop Number: {stop['Bus Stop Number']}</p>
-    //         <p>Bus Stop Name: {stop['Bus Stop Name']}</p>
-    //       </li>
-    //     ))}
-    //   </ul>
-    // </div>
-    // --------------------------------------------------------------
         <div>
             <h1>My Google Maps App</h1>
-            <ReverseBtn/>
-            <MapComponent busNumber={busNumber.toString()} busStops={busStops}/>
+            <ReverseBtn toggleBusDirection={toggleBusDirection} />
+            <MapComponent busNumber={busNumber.toString()} busStops={busStops} />
         </div>
     );
 }
