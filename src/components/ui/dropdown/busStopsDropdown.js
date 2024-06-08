787521030ca082644fd702042be01521203c1ba9
SteveJobs-2 <88567996+SteveJobs-2@users.noreply.github.com>
2024-06-08 15:54:43 +0800

    fix: Add limit to dropdown menu length

--- src/components/ui/dropdown/busStopsDropdown.js
+++ src/components/ui/dropdown/busStopsDropdown.js
@@ -17,6 +17,7 @@ const BusStopsDropdown = forwardRef((props, ref) => {
     // Context is available to all child components of the component which the context is defined in
     const { busStops } = useContext(BusStopsContext);
     console.log(busStops);
+
     return (
     <Dropdown>
         <Dropdown.Toggle id="dropdown-custom-1" variant='light'>
@@ -26,14 +27,10 @@ const BusStopsDropdown = forwardRef((props, ref) => {
         <Dropdown.Menu as={DropdownSearchMenu}>
 
             {busStops.map((busStop, index) => (
-                <Dropdown.Item eventKey={index}>{busStop['Full Name']}
+                <Dropdown.Item eventKey={index} key={index}>{busStop['Full Name']}
                 <Dropdown.Divider />
                 </Dropdown.Item>
             ))}
-            {/* <Dropdown.Item eventKey="1">Red</Dropdown.Item>
-            <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
-            <Dropdown.Item eventKey="3" active>Orange</Dropdown.Item>
-            <Dropdown.Item eventKey="1">Red-Orange</Dropdown.Item> */}
         </Dropdown.Menu>
     </Dropdown>
     )
--- src/components/ui/dropdown/dropdownSearchMenu.js
+++ src/components/ui/dropdown/dropdownSearchMenu.js
@@ -1,11 +1,22 @@
 // Import required libraries and functionalities
-import React, {forwardRef, useState} from 'react';
+import React, {useState} from 'react';
 import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Import required styling
+import Styles from './dropdownSearchMenu.module.css'; // Import the CSS file
+
 const DropdownSearchMenu = React.forwardRef(
     ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
-      const [value, setValue] = useState('');
+        const [value, setValue] = useState('');
+    
+        const filteredChildren = React.Children.toArray(children).filter((child) => {
+          // Ensure that child.props.children is a string before calling toLowerCase
+          if (typeof child.props.children === 'string') {
+            return !value || child.props.children.toLowerCase().includes(value.toLowerCase());
+          }
+          return true;
+        });
   
       return (
         <div
@@ -21,11 +32,10 @@ const DropdownSearchMenu = React.forwardRef(
             onChange={(e) => setValue(e.target.value)}
             value={value}
           />
-          <ul className="list-unstyled">
-            {React.Children.toArray(children).filter(
-              (child) =>
-                !value || child.props.children.toLowerCase().includes(value.toLowerCase())
-            )}
+          <ul className={`list-unstyled ${Styles.dropdownMenuScroll}`}>
+            {filteredChildren.map(child => (
+                <li key={child.key}>{child}</li>
+            ))}
           </ul>
         </div>
       );
