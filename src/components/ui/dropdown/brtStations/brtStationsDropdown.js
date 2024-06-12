283eb570e063a7d37ef5d35e601a5f0f14b5e628
luohr <luohr180@outlook.com>
2024-06-12 15:44:17 +0800

    feat: Finish functionality for brtStationsDropdown

--- src/components/ui/dropdown/brtStations/brtStationsDropdown.js
+++ src/components/ui/dropdown/brtStations/brtStationsDropdown.js
@@ -1,23 +1,36 @@
 // Import required libraries and functions
+import React, {useState} from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
+import brtRouteInformation from '../../../../data/brtRouteInfo';
 
 // Import required styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Import required context
+
 const BrtStationsDropdown = () => {
 
+    const brtRouteInfo = brtRouteInformation();
+    const [selectedBrtStation, setSelectedBrtStation] = useState('Brt Stations');
+
+    const handleSelect = (eventKey) => {
+        setSelectedBrtStation( Object.keys(brtRouteInfo)[eventKey] );
+
+    }
+
     return (
-        <Dropdown>
+        <Dropdown onSelect={handleSelect}>
             {/* If no valid search value, dropdown is disabled*/}
-            <Dropdown.Toggle id="dropdown-custom-1" variant='light' style={{minWidth: '10.85vw'}}>
-                {/* {selectedBusStopName.slice(0, 22)} */}
-                Brt Stations
+            <Dropdown.Toggle id="dropdown-custom-1" variant='light' style={{minWidth: '10vw'}}>
+                {selectedBrtStation.slice(0, 22)}
             </Dropdown.Toggle>
     
-            <Dropdown.Menu style={{maxWidth: '15vw'}}>
-                <Dropdown.Item>
-                    Test
-                </Dropdown.Item>
+            <Dropdown.Menu style={{minWidth: '10vw'}}>
+                {Object.keys(brtRouteInfo).map((brtRoute, index) => (
+                    <Dropdown.Item eventKey={index} key={index} active={selectedBrtStation === brtRoute}>
+                        {brtRoute}
+                    </Dropdown.Item>
+                ))}
             </Dropdown.Menu>
         </Dropdown>
         )
